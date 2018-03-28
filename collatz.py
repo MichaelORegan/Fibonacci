0380a64cd89eef4f715782221177ced7bad618f7
MichaelORegan <36163496+MichaelORegan@users.noreply.github.com>
2018-03-28 07:36:44 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -6,3 +6,8 @@ x = int(input("Please enter an integer: "))   # when run this will ask the user 
 if x <= 0:                                    # In case the entered number is 0 or less than 0 it will ask for a positive integer
   x = 0
   x = int(input("Please enter a positive integer: "))
+  
+  while x > 1:                                # While x is greater than 1
+    if x % 2 == 0:                            # if x is divisable by 2
+        x = x / 2                             # then divide x by 2
+        print(int(x))                         # print the new integer x
