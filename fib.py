4ce0b548b5566f0e5d26585dd85f43036c4705ad
MichaelORegan <36163496+MichaelORegan@users.noreply.github.com>
2018-04-03 15:33:26 +0100

    Update fib.py
    Bringing my forum posts from the readme file to under the codes

--- fib.py
+++ fib.py
@@ -25,6 +25,10 @@ ans = fib(x)
 
 print("Fibonacci number", x, "is", ans)  
 
+# The following is my post to the forum:
+# My name is Michael, so the first and last letter of my name (M + L = 13 +12 = 25), the 25th number of the fibonacci sequence is 75,025
+
+
 # Ian McLoughlin
 # A program that displays Fibonacci numbers using people's names.
 
@@ -53,3 +57,19 @@ print("My surname is", name)
 print("The first letter", first, "is number", firstno)
 print("The last letter", last, "is number", lastno)
 print("Fibonacci number", x, "is", ans)
+
+# Ord() function learning what it does and using it to get a larger Fibonacci number
+
+# The following is my post to the forum:
+# My surname is O'Regan The first letter O is number 79 The last letter n is number 110 Fibonacci number 189 is 1409869790947669143312035591975596518914
+
+# Ord() gives a character a counting numerical value, as per ascii O is 79 and n is 110
+
+# It took me a while to figure it out as I could not figure why they were not sequential numbers
+
+# This is shown best when I keep all letters in my surname in lower case as they become in sequence as in n then o as in the alphabet
+
+# My surname is o'regan The first letter o is number 111 The last letter n is number 110 Fibonacci number 221 is 6867260041627791953052057353082063289320596021
+
+# Similarly in all uppercase
+# My surname is O'REGAN The first letter O is number 79 The last letter N is number 78 Fibonacci number 157 is 289450641941273985495088042104137
