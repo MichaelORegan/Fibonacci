f1ed2f7f0bfece8b05a0182e0570c6074802303e
MichaelORegan <36163496+MichaelORegan@users.noreply.github.com>
2018-04-08 16:37:15 +0100

    Update Irisdataset.py
    adding extra code to print the other columns

--- Irisdataset.py
+++ Irisdataset.py
@@ -4,4 +4,5 @@
 
 with open("data/irisdataset.csv") as f:  # the with function opens and closes a file without the need to specify close the file
     for line in f:                       # the file is set to the arbitrary  value f above, for each line in f
-        print(line.split(',')[0])        # print column 0th (first column)
+        print((line.split(',')[0]), (line.split(',')[1]), (line.split(',')[2]), (line.split(',')[3]))        # print column 0th (first column), print second column, print third columnm, in each column we are taking out the comma and splitting it from the rest 
+      
