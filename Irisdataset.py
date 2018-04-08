aa17f8bc98e05af76c05dfe4e31e107e7fd9a91c
MichaelORegan <36163496+MichaelORegan@users.noreply.github.com>
2018-04-08 21:13:51 +0100

    Update Irisdataset.py
    formatting 5 spaces for each coluimn

--- Irisdataset.py
+++ Irisdataset.py
@@ -4,5 +4,7 @@
 
 with open("data/irisdataset.csv") as f:  # the with function opens and closes a file without the need to specify close the file
     for line in f:                       # the file is set to the arbitrary  value f above, for each line in f
-        print((line.split(',')[0]), (line.split(',')[1]), (line.split(',')[2]), (line.split(',')[3]))        # print column 0th (first column), print second column, print third columnm, in each column we are taking out the comma and splitting it from the rest 
+        print(('{:^5}'.format(line.split(',')[0])), ('{:^5}'.format(line.split(',')[1])), ('{:^5}'.format(line.split(',')[2])), ('{:^5}'.format(line.split(',')[3])))        
+        # print column 0th (first column), print second column, print third columnm, in each column we are taking out the comma and splitting it from the rest, each column in in the middel of 5 spaces 
+# https://pyformat.info/ 
       
